abfbbf3d485b2a33c1be49a5718f341897293f5e
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-09-25 20:22:11 -0600

    Creacion de los TDA a utilizar

---
+++ IPC2_Proyecto2_202405516/models/Cola.py
@@ -0,0 +1,42 @@
+from .Nodo import Nodo
+
+# Para procesar instrucciones en orden de llegada plan de riego y simulación
+
+class Cola:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.size = 0
+
+    def encolar(self, dato):
+        nuevo = Nodo(dato)
+        if self.primero is None:
+            self.primero = self.ultimo = nuevo
+        else: 
+            self.ultimo.siguiente = nuevo
+            self.ultimo = nuevo
+
+    def desencolar(self):
+        if self.primero is None:
+            return None
+        actual = self.primero
+        self.primero = self.primero.siguiente
+        if self.primero is None:
+            self.ultimo = None
+        self.size -= 1
+        return actual.dato
+    
+    def estaVacia(self):
+        if self.primero is None:
+            return True
+        else: 
+            return False
+        
+    def __len__(self):
+        return self.size
+    
+    def __iter__(self):
+        actual = self.primero
+        while actual:
+            yield actual.dato
+            actual = actual.siguiente
---
+++ IPC2_Proyecto2_202405516/models/ListaEnlazada.py
@@ -0,0 +1,35 @@
+from .Nodo import Nodo
+
+#Utilizado para cualquier colección donde tenga que recorrer, agregar o eliminar elementos dinámicamente.
+
+class ListaEnlazada:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.size = 0
+
+    def insertar(self, dato):
+        nuevo = Nodo(dato)
+        if self.primero is None:
+            self.primero = self.ultimo = nuevo
+        else:
+            self.ultimo.siguiente = nuevo
+            self.ultimo = nuevo
+        self.size += 1
+
+    def buscar(self, dato):
+        actual = self.primero
+        while actual:
+            if actual.dato == dato:
+                return actual
+            actual = actual.siguiente
+        return None
+    
+    def __len__(self):
+        return self.size
+    
+    def __iter__(self):
+        actual = self.primero
+        while actual:
+            yield actual.dato
+            actual = actual.siguiente
