6df462536db871fb7496f33ef94c4bcd83a5ea5a
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-09-27 00:40:21 -0600

    Funcionas para Hileras

--- IPC2_Proyecto2_202405516/models/Hilera.py
+++ IPC2_Proyecto2_202405516/models/Hilera.py
@@ -6,3 +6,16 @@ class Hilera:
         self.plantas_x_hilera = plantas_x_hilera
         self.plantas = ListaEnlazada()
         self.dron_asignado = None
+
+    def agregar_planta(self, planta):
+        if planta.hilera == self.numero:
+            self.plantas.insertar(planta)
+
+    def obtener_planta(self, posicion):
+        for planta in self.plantas:
+            if planta.posicion == posicion:
+                return planta
+        return None
+    
+    def __str__(self):
+        return f"Hilera {self.numero} ({len(self.plantas)} plantas)"
