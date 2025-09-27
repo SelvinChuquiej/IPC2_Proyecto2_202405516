9b3e1d2a8e4cb424548c647ade5d8c7eaf372315
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-09-27 00:57:27 -0600

    Funciones para Invernadero y PlanRiego

--- IPC2_Proyecto2_202405516/models/Invernadero.py
+++ IPC2_Proyecto2_202405516/models/Invernadero.py
@@ -1,4 +1,5 @@
 from models.ListaEnlazada import ListaEnlazada
+from models.Hilera import Hilera
 
 class Invernadero:
     def __init__(self, nombre, num_hileras, plantas_x_hilera):
@@ -8,3 +9,26 @@ class Invernadero:
         self.hileras = ListaEnlazada()
         self.drones = ListaEnlazada()
         self.planes_riego = ListaEnlazada() 
+
+        for i in range(1, num_hileras + 1):
+            self.hileras.insertar(Hilera(i, plantas_x_hilera))
+
+    def agregar_planta(self, planta):
+        for hilera in self.hileras:
+            if hilera.numero == planta.hilera:
+                hilera.agregar_planta(planta)
+                return hilera
+        return None
+
+    def obtener_hilera(self, numero_hilera):
+        for hilera in self.hileras:
+            if hilera.numero == numero_hilera:
+                return hilera
+        return None
+    
+    def asignar_dron(self, dron, numero_hilera):
+        hilera = self.obtener_hilera(numero_hilera)
+        if hilera:
+            dron.hilera_asignada = numero_hilera 
+            hilera.dron_asignado = dron
+            self.drones.insertar(dron)
--- IPC2_Proyecto2_202405516/models/PlanRiego.py
+++ IPC2_Proyecto2_202405516/models/PlanRiego.py
@@ -7,3 +7,34 @@ class PlanRiego:
         self.instrucciones_original = instrucciones_texto
         self.cola_instrucciones = Cola()
         self.instrucciones_procesadas = ListaEnlazada()
+        self.cargar_instrucciones(instrucciones_texto)
+
+    def cargar_instrucciones(self, texto):
+        instrucciones = texto.split(", ")
+        for inst in instrucciones:
+            inst_limpia = inst.strip()
+            if self.validar_formato(inst_limpia):
+                self.cola_instrucciones.encolar(inst_limpia)
+    
+    def validar_formato(self, instruccion):
+        # Validar que sea "HX-PY"
+        partes = instruccion.split('-')
+        if len(partes) != 2:
+            return False
+        return partes[0].startswith('H') and partes[1].startswith('P')
+    
+    def siguiente_instruccion(self):
+        if not self.cola_instrucciones.esta_vacia():
+            inst = self.cola_instrucciones.desencolar()
+            self.instrucciones_procesadas.agregar(inst)
+            return inst
+        return None
+    
+    def instrucciones_restantes(self):
+        return len(self.cola_instrucciones)
+    
+    def esta_completado(self):
+        return self.cola_instrucciones.esta_vacia()
+    
+    def __str__(self):
+        return f"Plan '{self.nombre}' - Instrucciones: {self.instrucciones_original}"
