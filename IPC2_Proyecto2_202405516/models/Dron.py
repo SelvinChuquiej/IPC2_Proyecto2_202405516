b7bbb852bb3f34020494feddd799d694d4287aab
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-09-27 00:25:18 -0600

    Funciones basicas para los Drones

--- IPC2_Proyecto2_202405516/models/Dron.py
+++ IPC2_Proyecto2_202405516/models/Dron.py
@@ -7,3 +7,28 @@ class Dron:
         self.agua_usada = 0
         self.fertilizante_usado = 0
         self.estado = "inicio"
+
+    def mover_adelante(self):
+        self.posicion_actual += 1
+        self.estado = "moviendo"
+        return f"Adelante H{self.hilera_asignada}-P{self.posicion_actual}"
+
+    def mover_atras(self):
+        self.posicion_actual -= 1
+        self.estado = "moviendo"
+        return f"Atras H{self.hilera_asignada}-P{self.posicion_actual}"
+
+    def regar_planta(self, planta):
+        self.agua_usada += planta.lt_agua
+        self.fertilizante_usado += planta.gr_fertilizante
+        self.estado = "regando"
+        planta.regada = True
+        return f"Regar planta H{planta.hilera}-P{planta.posicion}"
+
+    def esperar(self):
+        self.estado = "esperando"
+        return "Esperar"
+    
+    def finalizar(self):
+        self.estado = "finalizado"
+        return "Finalizar"
